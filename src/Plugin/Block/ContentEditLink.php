689e58558695e0ea01aa9c56de4c4a19b7076a4e
Brian Arsenault <barsenault@fsu.edu>
2020-02-04 15:56:02 -0500

    Adds a Block containing an edit link for the custom content types

---
+++ src/Plugin/Block/ContentEditLink.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\ldbase_handlers\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * @Block(
+ *   id = "content_edit_link",
+ *   admin_label = @Translation("LDBase Content Edit Link"),
+ *   category = @Translation("LDBase Menu"),
+ *   context = {
+ *     "node" = @ContextDefinition(
+ *       "entity:node",
+ *       label = @Translation("Current Node")
+ *     )
+ *   }
+ * )
+ */
+class ContentEditLink extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = $this->getContextValue('node');
+    $node_type = $node->getType();
+    $nid = $node->id();
+
+    $markup = '';
+    $route = 'ldbase_handlers.edit_' . $node_type;
+    $text = 'Edit';
+
+    $url = Url::fromRoute($route, array('node' => $nid));
+    $link = Link::fromTextAndUrl(t($text), $url)->toRenderable();
+    $markup .= render($link) . ' ';
+
+    $block = [
+      '#type' => 'markup',
+      '#markup' => $markup,
+    ];
+
+    return $block;
+  }
+}
