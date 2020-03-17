a3250f6423cc3d3e5fd724a2a982ab95ad547acc
Brian Arsenault <barsenault@fsu.edu>
2020-03-17 14:44:37 -0400

    adds Project Tasks links and routing

---
+++ src/Plugin/Menu/ProjectTasks.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\ldbase_handlers\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * menulink test
+ */
+class ProjectTasks extends MenuLinkDefault {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRouteParameters() {
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      return ['node' => $node->id()];
+    }
+    else {
+      return ['node' => 999];
+    }
+
+  }
+}
