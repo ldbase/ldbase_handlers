410b3c2604161c589f11e992f3751e502e83bba0
Brian Arsenault <barsenault@fsu.edu>
2020-07-10 11:54:24 -0400

    adds views access plugin that uses LDbase access checker

---
+++ src/Plugin/views/access/LDbaseViewsCustomAccess.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\ldbase_handlers\Plugin\views\access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\views\Plugin\views\access\AccessPluginBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\Entity\Node;
+use Symfony\Component\Routing\Route;
+
+
+/**
+ * Class LDbaseViewsCustomAccess
+ *
+ * @ingroup views_access_plugins
+ *
+ * @ViewsAccess(
+ *  id = "LDbaseViewsCustomAccess",
+ *  title = @Translation("Customized LDbase Access for views"),
+ *  help = @Translation("Adds Group Update Node Access Check"),
+ * )
+ */
+
+class LDbaseViewsCustomAccess extends AccessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summaryTitle() {
+    return $this->t("LDbase Group Update Node Access");
+  }
+
+  public function access(AccountInterface $account) {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    return \Drupal::service('group_content.update_access_checker')->access($account, $node);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRouteDefinition(Route $route) {
+    // add access check and parameters to route
+    $route->setRequirement('_group_update_node_access_check', 'TRUE');
+    $route->setOption('parameters', array('uuid' => ['type' => 'ldbase_uuid'], 'node' => ['type' => 'entity:node']));
+  }
+
+}
