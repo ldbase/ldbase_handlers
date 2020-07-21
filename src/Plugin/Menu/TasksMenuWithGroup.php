e3a04c579d7e233da7ad4865848b31221e0a2c53
Brian Arsenault <barsenault@fsu.edu>
2020-07-21 16:20:35 -0400

    adds User Management link to Project Tasks

---
+++ src/Plugin/Menu/TasksMenuWithGroup.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\ldbase_handlers\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+use Drupal\Core\Cache\Cache;
+use Drupal\group\Entity\GroupContent;
+
+/**
+ * Gets the nid for the currently viewed node and assigns it to a
+ * route parameter.
+ */
+class TasksMenuWithGroup extends MenuLinkDefault {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), array('user'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRouteParameters() {
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+        $group_contents = GroupContent::loadByEntity($node);
+        $group = array_pop($group_contents)->getGroup();
+
+      return ['node' => $node->uuid(), 'group' => $group->id()];
+    }
+    else {
+      // if no nid, pass some text to keep Structure > Menus > Edit Menu from breaking
+      return ['node' => 'not_a_node'];
+    }
+
+  }
+
+  public function getCacheMaxAge() {
+    return 0;
+  }
+}
