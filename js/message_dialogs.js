617b448609e51bc3d92db5d0df63e51e58c88cfd
Brian Arsenault <barsenault@fsu.edu>
2021-09-09 17:23:15 -0400

    reload dashboard on message close

--- js/message_dialogs.js
+++ js/message_dialogs.js
@@ -5,14 +5,17 @@
       var uid = drupalSettings.messageData.uid;
 
       $(window)
-      .once('message_dialogs')
-      .on('dialog:aftercreate', function (event, dialog, $element) {
+        .once('message_dialogs')
+        .on('dialog:aftercreate', function (event, dialog, $element) {
 
-        var url = '/ajax/message/' + mid + '/' + uid + '/mark-read';
-        Drupal.ajax({url: url}).execute();
+          var url = '/ajax/message/' + mid + '/' + uid + '/mark-read';
 
-        $("tr.message-row-" + mid).find("a").removeClass("message-status-unread");
-      });
+          Drupal.ajax({url: url}).execute();
+
+        })
+        .on('dialog:beforeclose', function (event, dialog, $element){
+          location.reload();
+        })
     }
   };
 })(jQuery, Drupal);
